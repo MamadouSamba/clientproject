d8f66118c2e905a96ac6870bd64948c0b06437ef
Mamadou Samba <mamadou02@outlook.com>
2019-05-06 21:52:15 +0000

    Prog

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@
 
 
 $('button').click(function(){
+    var UserInput=$("input").val();
+    var searchInput="http://api.openweathermap.org/data/2.5/weather?q=" + UserInput + "&APPID=3bb00f30e525b91a1deb9cbd20254379"
     $.ajax({
-        url:"https://api.openweathermap.org/data/2.5/weather?zip=94040,us",
+        url:"https://api.openweathermap.org/data/2.5/weather?q=" + UserInput + "&APPID=3bb00f30e525b91a1deb9cbd20254379",
         method:"GET",
         success:function(response){
-            console.log("ok")
+            console.log(response.weather[0])
         }
     })
 
