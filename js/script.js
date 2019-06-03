76544acf56ece5e142069af0c3daac866adfbdf5
Mamadou Samba <mamadou02@outlook.com>
2019-06-03 21:35:18 +0000

    MVP

--- js/script.js
+++ js/script.js
@@ -10,8 +10,10 @@ $('button').click(function(){
         method:"GET",
         success:function(response){
             var weather=response.weather[0].description;
-            console.log(weather)
-            $(".storm").html(weather)
+            console.log(weather);
+            var precipitate=$("input").val();
+            $("#solar").html(precipitate);
+            $(".storm").html(weather);
             if (weather.includes("sun") || weather.includes("clear")) {
                 $("body").css("background-image", "url('https://media.apnarm.net.au/media/images/2017/08/11/b88916986z1_20170811171219_000guro78c72-0-1nzfq5wlk7v5k9idqo2_ct677x380.jpg ')");
             } else {
