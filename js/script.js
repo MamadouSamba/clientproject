4ec45d61a7fb7f2dee645b6d01b5081b88c2b891
Mamadou Samba <mamadou02@outlook.com>
2019-06-03 21:04:05 +0000

    MVP

--- js/script.js
+++ js/script.js
@@ -12,14 +12,20 @@ $('button').click(function(){
             var weather=response.weather[0].description;
             console.log(weather)
             $(".storm").html(weather)
-            if(weather.includes("sun")||weather.includes("clear")){
+            if (weather.includes("sun") || weather.includes("clear")) {
                 $("body").css("background-image", "url('https://media.apnarm.net.au/media/images/2017/08/11/b88916986z1_20170811171219_000guro78c72-0-1nzfq5wlk7v5k9idqo2_ct677x380.jpg ')");
-               } else {
-                    $("body").css("background-image", "url('https://www.cbc.ca/doczone/content/images/episodes/waethergonewiild_1280.jpg')");
-                }
-
+            } else {
+                $("body").css("background-image", "url('https://www.cbc.ca/doczone/content/images/episodes/waethergonewiild_1280.jpg')");
             }
-        }
-    )
+            if (weather.includes("snow")){
+                $("body").css("background-image", "url('https://media.graytvinc.com/images/810*453/Snow431.JPG')");
+            } else {
+                $("body").css("background-image", "url('https://marketplace.canva.com/MAB-lSDUHIk/1/thumbnail_large/canva-blue-clear-sky-abstract-with-copyspace-MAB-lSDUHIk.jpg')");
+            }
+            
+            
 
+            
+        }
+    })
 });
